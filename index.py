aa8c0eb994dcedd647236102a59ea9012a4792b6
jay23cee <jayceexxiii@gmail.com>
2019-08-13 23:47:26 -0700

    Initial  commit

---
+++ index.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+my_dataList = [2,4,6]
+
+
+
+ar = np.array(my_dataList)
+
+print(ar)
+
+# assigning nested list, which is a 2d format
+my_mat = [[1,2,3], [4,5,6], [7,8,9]]
+
+# Cast the list variable to an rray() and use the library np
+
+print(np.array(my_mat))
+
